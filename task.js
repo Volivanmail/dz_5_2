79ef385fed7a784fe49e1b1ab128fa0a283dbeb3
Volivanmail <84474430+Volivanmail@users.noreply.github.com>
2022-07-05 00:43:06 +0300

    2

--- task.js
+++ task.js
@@ -1,16 +1,16 @@
-const cl = document.getElementById('clicker__counter');
+const counter = document.getElementById('clicker__counter');
 const img = document.getElementById('cookie');
 
-// const click = Number(cl.textContent);
+let clicks = Number(counter.textContent);
+img.onclick = cc;
 
-img.onclick = cc()
-
-function cc () {
-    cl.textContent = Number(cl.textContent) += 1;
-    if (Number(cl.textContent) % 2 == 0) {
+function cc() {
+    
+    if (Number(counter.textContent) % 2 == 0) {
       img.width += 100;
     }
     else {
       img.width -= 100;
     }
+    counter.textContent = clicks+=1;
 }
